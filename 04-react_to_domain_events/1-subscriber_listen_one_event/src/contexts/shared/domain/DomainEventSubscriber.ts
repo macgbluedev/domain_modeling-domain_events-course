3f1092e54f072e295381af310ea6a4e67d32891b
Moisés Croes <45533833+macgbluedev@users.noreply.github.com>
2026-08-11 02:18:35 +0200

    Domain event sub contract is in domain layer, return a void because we have the premise that generate a side effect

--- 04-react_to_domain_events/1-subscriber_listen_one_event/src/contexts/shared/domain/DomainEventSubscriber.ts
+++ 04-react_to_domain_events/1-subscriber_listen_one_event/src/contexts/shared/domain/DomainEventSubscriber.ts
@@ -1,7 +1,7 @@
 import { DomainEvent } from "./DomainEvent";
 import { DomainEventName } from "./DomainEventName";
 
-export interface DomainEventSubscriber<T extends DomainEvent> {
+export interface DomainEventSubscriber<T extends DomainEvent> { //domain event sub contract is in domain layer, return a void because we have the premise that generate a side effect
 	on(domainEvent: T): Promise<void>;
 
 	subscribedTo(): DomainEventName<T>[];
